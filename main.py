55f402148f00807f689d1a4579cd4e11865790b1
changeonPark <qkrcksrjs@gmail.com>
2023-04-21 11:52:53 +0900

    feat: get pagination

--- main.py
+++ main.py
@@ -7,33 +7,54 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
 
-browser.get(f"https://www.indeed.com/jobs?q=python&limit=10")
+def get_page_count(keyword):
+    url = f"https://kr.indeed.com/jobs?q={keyword}&limit=10"
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get(url)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.find("nav", {"aria-label": "pagination"}).findAll("div")
 
-soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
-jobs = job_list.find_all("li", recursive=False)
-results = []
+    if len(pagination) == 0:
+        print("none")
+        return 0
+    else:
+        print(f"has {len(pagination) - 1}")
+        return len(pagination) - 1
 
-for job in jobs:
-    zone = job.find("div", class_="mosaic-zone")
-    if zone == None:
-        anchor = job.select_one("h2 a")
-        title = anchor["aria-label"]
-        link = anchor["href"]
-        company = job.find("span", class_="companyName")
-        location = job.find("div", class_="companyLocation")
-        job_data = {
-            "link": f"https://kr.indeed.com{link}",
-            "company": company.string,
-            "location": location.string,
-            "position": title,
-        }
-        results.append(job_data)
 
-for result in results:
-    print(result, "\n////")
+get_page_count("python")
 
-while True:
-    pass
+
+def extract_indeed_jobs():
+    base_url = "https://kr.indeed.com/jobs?q="
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    browser.get(f"{base_url}python&limit=10")
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all("li", recursive=False)
+    results = []
+
+    for job in jobs:
+        zone = job.find("div", class_="mosaic-zone")
+        if zone == None:
+            anchor = job.select_one("h2 a")
+            title = anchor["aria-label"]
+            link = anchor["href"]
+            company = job.find("span", class_="companyName")
+            location = job.find("div", class_="companyLocation")
+            job_data = {
+                "link": f"https://kr.indeed.com{link}",
+                "company": company.string,
+                "location": location.string,
+                "position": title,
+            }
+            results.append(job_data)
+
+    for result in results:
+        print(result, "\n////")
+
+    while True:
+        pass

d22eba51652f3b65456ba613d2dbcec289ad96d4
changeonPark <qkrcksrjs@gmail.com>
2023-04-21 14:44:23 +0900

    feat: flask render html

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("ChanScrapper")
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", name="changeon", title="ChanScrapper")
+    return render_template("index.html")
 
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+@app.route("/search")
+def search():
+    return render_template("search.html")
 
 
 app.run(host="0.0.0.0", port=3001)

d715049175ba6751923fc77c94389ca317abbae1
changeonPark <qkrcksrjs@gmail.com>
2023-04-21 14:49:59 +0900

    feat: flask get arguments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 
@@ -12,7 +12,11 @@ def home():
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
 app.run(host="0.0.0.0", port=3001)
